5bece9fa666884eedafd4c168a93c48f6bf83037
vluciano12 <cluciano@stu.socsd.org>
2022-03-15 08:57:42 -0400

    HOW DO SUB AND SUPER CLASSES WORK HELP MEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEE

---
+++ Animal.java
@@ -0,0 +1,21 @@
+public class Animal{
+    private String species;
+    private String sciName;
+    private double avgSize;
+
+    public Animal(String sp, String sc, double avg){
+        species = sp;
+        sciName = sc;
+        avgSize = avg;
+    }
+
+    public String toString(){
+        return "I am a " + species + ", also known as a " + sciName + ". My average size is " + avgSize + ".";
+    }
+}
+
+public class Bird extends Animal{
+    
+    private int wingSpan;
+    
+}
